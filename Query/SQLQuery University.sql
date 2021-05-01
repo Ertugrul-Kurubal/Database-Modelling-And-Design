fbd125abcdad94c9d10729893132c1a88623a571
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-05-01 23:43:58 +0300

    University Project

---
+++ Query/SQLQuery University.sql
@@ -0,0 +1,110 @@
+
+
+CREATE DATABASE University;
+
+USE University;
+
+CREATE TABLE Register 
+(
+StudentID bigint,
+[Name] varchar(50),
+Surname varchar(50),
+RegisterDate date,
+[Date] datetime,
+[Location] nvarchar(50)
+PRIMARY KEY (StudentID)
+);
+
+CREATE TABLE Staff 
+(
+StaffID bigint,
+[Name] varchar(50),
+Surname varchar(50),
+[Location] nvarchar(50),
+Job varchar(50)
+PRIMARY KEY (StaffID)
+);
+
+CREATE FUNCTION dbo.homework
+(
+	@NUM INT
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @OUT INT
+	DECLARE @COURSECREDIT INT
+	SET @COURSECREDIT = (SELECT Credit FROM Course WHERE Credit = @NUM)
+	IF @COURSECREDIT > 15
+		SET @OUT = 5
+	ELSE
+		SET @OUT = 3
+RETURN @OUT
+END;
+
+--------------------------------------------------------------------------------
+
+CREATE TABLE Course 
+(
+CourseID bigint PRIMARY KEY,
+[Name] varchar(20),
+Credit int,
+Homework int,
+Quota int,
+CONSTRAINT Homework CHECK(Homework < [dbo].[homework](Credit))
+);
+
+CREATE TABLE CourseScore 
+(
+CourseID bigint,
+CONSTRAINT StudentID bigint CHECK(),
+Score int
+FOREIGN KEY (CourseID) REFERENCES Course (CourseID),
+FOREIGN KEY (StudentID) REFERENCES Register (StudentID)
+);
+
+CREATE FUNCTION dbo.loc
+(
+	@LOC bigint
+)
+RETURNS bit
+AS
+BEGIN
+	DECLARE @STLOC bigint
+	DECLARE @STFLOC bigint
+	DECLARE @STAT bit
+	SET @STLOC = (SELECT [Location] FROM Register WHERE StudentID = @LOC)
+	SET @STFLOC = (SELECT [Location] FROM Staff WHERE StaffID = (SELECT StaffID FROM Conselor WHERE StudentID = @LOC)
+	IF @STLOC = @STFLOC
+		SET @STAT = TRUE
+	ELSE
+		SET @STAT = FALSE
+RETURN @STAT
+END;
+
+
+CREATE TABLE Conselor 
+(
+StudentID bigint,
+CONSTRAINT StaffID bigint CHECK(dbo.loc(StudentID)),
+Region int
+FOREIGN KEY (StudentID) REFERENCES Register (StudentID),
+FOREIGN KEY (StaffID) REFERENCES Staff (StaffID)
+);
+
+CREATE TABLE CourseLecturer 
+(
+StaffID bigint,
+CourseID bigint
+FOREIGN KEY (StaffID) REFERENCES Staff (StaffID),
+FOREIGN KEY (CourseID) REFERENCES Course (CourseID)
+);
+
+-----------------------------------------------------------------------------------------
+
+INSERT Register(StudentID,[Name], Surname, RegisterDate,[Date], [Location]) 
+VALUES (201910010,'Arthur','Rose', 2019, 05/09/2019, 'England'),
+	   (201910017,'Beth', 'Mac', 2019,	05/09/2019,	'Scotland'),
+	   (201910024,'David',	'Kahnmar', 2019, 07/09/2019,'Northern Ireland'),
+	   (201910043,	'Rosa',	'Johnson', 2019, 08/09/2019, 'Scotland')
+		
