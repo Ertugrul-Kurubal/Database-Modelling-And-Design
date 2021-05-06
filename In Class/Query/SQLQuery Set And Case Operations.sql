eebbf94f338ae73406539b0c8e6cc4612b955c49
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-05-06 21:18:48 +0300

    Example 1 Solution

--- In Class/Query/SQLQuery Set And Case Operations.sql
+++ In Class/Query/SQLQuery Set And Case Operations.sql
@@ -1,3 +1,42 @@
 ------ SET OPERATIONS ------
 -- UNION / UNION ALL
 -- Fairport þehrindeki müþteriler ile East Meadow þehrindeki müþterilerin soyisimlerini listeleyin
+
+SELECT last_name 
+FROM [sales].[customers]
+WHERE city = 'Fairport'
+UNION
+SELECT last_name 
+FROM [sales].[customers]
+WHERE city = 'East Meadow'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
