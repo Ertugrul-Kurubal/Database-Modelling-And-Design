4f5334e238a0e04d7a97577b57f3be9a58d16e7f
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-05-06 22:53:13 +0300

    Simple Case Condition

--- In Class/Query/SQLQuery Set And Case Operations.sql
+++ In Class/Query/SQLQuery Set And Case Operations.sql
@@ -126,6 +126,103 @@ WHERE customer_id IN (SELECT customer_id FROM [sales].[orders]
 					WHERE DATEPART(year, order_date) = 2018
 					)
 
+-- 2016 model bisiklet markalarýndan hangilerinin 2017 model bisikleti yoktur?
+-- brand_id ve brand_name deðerlerini listeleyin
+
+SELECT brand_id 
+FROM [production].[products]
+WHERE model_year = 2016
+EXCEPT
+SELECT brand_id 
+FROM [production].[products]
+WHERE model_year = 2017
+
+SELECT * 
+FROM [production].[brands]
+WHERE brand_id IN (SELECT brand_id 
+					FROM [production].[products]
+					WHERE model_year = 2016
+					EXCEPT
+					SELECT brand_id 
+					FROM [production].[products]
+					WHERE model_year = 2017
+					)
+
+-- Sadece 2017 yýlýnda satýlan, diðer yýllarda satýlmayan bisikletleri listeleyin.
+-- Bu kurala uyan bisiklet varsa product_id ve product_name bilgilerini listeleyin
+
+SELECT B.product_id 
+FROM [sales].[orders] A, [sales].[order_items] B
+WHERE YEAR(order_date) = 2017 AND A.order_id = B.order_id
+EXCEPT
+SELECT B.product_id 
+FROM [sales].[orders] A, [sales].[order_items] B
+WHERE A.order_id = B.order_id AND YEAR(order_date) <> 2017
+
+SELECT product_id, product_name 
+FROM [production].[products]
+WHERE product_id IN (SELECT B.product_id 
+			FROM [sales].[orders] A, [sales].[order_items] B
+			WHERE YEAR(order_date) = 2017 AND A.order_id = B.order_id
+			EXCEPT
+			SELECT B.product_id 
+			FROM [sales].[orders] A, [sales].[order_items] B
+			WHERE A.order_id = B.order_id AND YEAR(order_date) <> 2017
+			)
+
+SELECT product_id, product_name 
+FROM [production].[products]
+WHERE product_id IN (SELECT B.product_id 
+			FROM [sales].[orders] A, [sales].[order_items] B
+			WHERE YEAR(order_date) = 2017 AND A.order_id = B.order_id
+			EXCEPT
+			SELECT B.product_id 
+			FROM [sales].[orders] A, [sales].[order_items] B
+			WHERE A.order_id = B.order_id AND (YEAR(order_date) < 2017 OR 
+			YEAR(order_date) > 2017))
+
+------ CASE EXPRESSION ------
+-- Simple Case Expression -----
+-- Order status id lerinin açýklamalarýný yazdýrýnýz.
+-- 1 = Pending; 2 = Processing; 3 = Rejected; 4 = Completed
+
+SELECT *, 
+	CASE order_status 
+		WHEN 1 THEN 'Pending'
+		WHEN 2 THEN 'Processing'
+		WHEN 3 THEN 'Rejected'
+		WHEN 4 THEN 'Completed'
+		ELSE 'Undefined'
+	END AS order_def
+FROM [sales].[orders]
+;
+
+-- Personellerin çalýþtýklarý maðaza isimlerini simple case expression ile yazdýrýnýz.
+
+SELECT *, 
+		CASE store_id
+			WHEN 1 THEN 'Santa Cruz Bikes'
+			WHEN 2 THEN 'Baldwin Bikes'
+			WHEN 3 THEN 'Rowlett Bikes'
+			ELSE 'Undefined'
+		END AS store_name
+FROM [sales].[staffs]
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
