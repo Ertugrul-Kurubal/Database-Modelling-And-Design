43cb06324ecad3433083fb0708e1b9558f517432
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-05-06 21:26:20 +0300

    Qury time question

--- In Class/Query/SQLQuery Set And Case Operations.sql
+++ In Class/Query/SQLQuery Set And Case Operations.sql
@@ -17,10 +17,31 @@ UNION ALL
 SELECT last_name 
 FROM [sales].[customers]
 WHERE city = 'East Meadow'
+ORDER BY last_name 
 
+-- IN logical operatörü kullanýlarak da yapýlabilir.
 
+SELECT	last_name
+FROM	sales.customers
+WHERE	city IN ('Fairport', 'East Meadow')
 
+-- IN logical operatörü kullanýlarak da yapýlabilir.
 
+SELECT DISTINCT	last_name
+FROM	sales.customers
+WHERE	city IN ('Fairport', 'East Meadow')
+
+-- Veritabanýmýzda bulunan bütün isim soyisimleri listeleyin.
+-- Sonuçta sadece ad ve soyad olmak üzere iki sütun olacak.
+-- UNION ve UNION ALL sonuçlarýný karþýlaþtýrýn.
+
+SELECT first_name, last_name FROM [sales].[staffs]
+UNION
+SELECT first_name, last_name FROM [sales].[customers]
+
+SELECT first_name, last_name FROM [sales].[staffs]
+UNION ALL
+SELECT first_name, last_name FROM [sales].[customers]
 
 
 
