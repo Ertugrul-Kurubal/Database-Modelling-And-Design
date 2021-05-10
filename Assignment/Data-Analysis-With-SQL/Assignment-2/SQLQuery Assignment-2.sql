888e2cb0bdae6c402f056120283b18d2e264f915
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-05-10 23:30:12 +0300

    With Table Checked

--- Assignment/Data-Analysis-With-SQL/Assignment-2/SQLQuery Assignment-2.sql
+++ Assignment/Data-Analysis-With-SQL/Assignment-2/SQLQuery Assignment-2.sql
@@ -10,14 +10,14 @@ WITH transactions  AS
 		FROM 
 		(
 			VALUES
-			(
 			(5, 2, 10, CAST('02-12-20' AS date)),
 			(1, 3, 15, CAST('02-13-20' AS date)), 
 			(2, 1, 20, CAST('02-13-20' AS date)), 
 			(2, 3, 25, CAST('02-14-20' AS date)), 
 			(3, 1, 20, CAST('02-15-20' AS date)), 
 			(3, 2, 15, CAST('02-15-20' AS date)), 
-			(1, 4, 5,  CAST('02-16-20' AS date)))		    )
+			(1, 4, 5,  CAST('02-16-20' AS date))
+			)
 		AS Table_1 ([sender], receiver, amount, [transaction-date]) 
 )
 SELECT * 
