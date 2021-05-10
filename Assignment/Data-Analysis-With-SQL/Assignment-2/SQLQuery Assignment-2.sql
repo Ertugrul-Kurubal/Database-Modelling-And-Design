20b87d3720f81aadecd6c544433bd92f7f177d20
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-05-10 22:50:09 +0300

    Q1 With Clause

--- Assignment/Data-Analysis-With-SQL/Assignment-2/SQLQuery Assignment-2.sql
+++ Assignment/Data-Analysis-With-SQL/Assignment-2/SQLQuery Assignment-2.sql
@@ -3,7 +3,24 @@
 --Assignment-2
 
 --Q1
-
+WITH users (User_id, [Action], [date]) AS 
+(
+SELECT 1, 'Start', '2020-01-01'
+UNION ALL
+SELECT 1, 'Cancel','2020-02-01'
+UNION ALL
+SELECT 2, 'Start', '2020-03-01'
+UNION ALL
+SELECT 2, 'Publish', '2020-04-01'
+UNION ALL
+SELECT 3, 'Start', '2020-05-01'
+UNION ALL
+SELECT 3, 'Cancel', '2020-06-01'
+UNION ALL 
+SELECT 1, 'Start', '2020-07-01'
+UNION ALL
+SELECT 1, 'Publish', '2020-08-01'
+)
 
 
 
