d669d16603b858926a0903a15d0713c064b1f70c
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-05-10 23:03:23 +0300

    Q1 Ordinary CTE

--- Assignment/Data-Analysis-With-SQL/Assignment-2/SQLQuery Assignment-2.sql
+++ Assignment/Data-Analysis-With-SQL/Assignment-2/SQLQuery Assignment-2.sql
@@ -3,23 +3,25 @@
 --Assignment-2
 
 --Q1
-WITH transactions (
- SELECT * 
- FROM 
- (
- VALUES(
- (1, 'Start', CAST('01-01-20' AS date),
- (1, 'Cancel', CAST('01-02-20' AS date),
- (2, 'Start', CAST('01-03-20' AS date),
- (2, 'Publish', CAST('01-04-20' AS date),
- (3, 'Start', CAST('01-05-20' AS date),
- (3, 'Cancel', CAST('01-06-20' AS date),
- (1, 'Start', CAST('01-07-20' AS date),
- (1, 'Publish', CAST('01-08-20' AS date)
-  AS Table_1 ([user_id], [action], [date])(sender, receiver, amount, transaction-date) AS 
-(
 
+WITH transactions  AS
+(
+		SELECT * 
+		FROM 
+		(
+			VALUES(
+			(1,'start', CAST('01-01-20' AS date)),
+			(1,'cancel', CAST('01-02-20' AS date)), 
+			(2,'start', CAST('01-03-20' AS date)), 
+			(2,'publish', CAST('01-04-20' AS date)), 
+			(3,'start', CAST('01-05-20' AS date)), 
+			(3,'cancel', CAST('01-06-20' AS date)), 
+			(1,'start', CAST('01-07-20' AS date)), 
+			(1,'publish', CAST('01-08-20' AS date))
+		)
+		AS Table_1 (sender, receiver, amount, [transaction-date]) 
 )
+
 
 
 
