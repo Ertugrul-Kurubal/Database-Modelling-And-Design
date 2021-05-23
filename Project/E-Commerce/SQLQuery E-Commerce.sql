5e9ae83c086cbd590f7ff2a45c97a5115e474d76
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-05-23 18:18:04 +0300

    Creating Tables

--- Project/E-Commerce/SQLQuery E-Commerce.sql
+++ Project/E-Commerce/SQLQuery E-Commerce.sql
@@ -1,3 +1,29 @@
-USE master
+--E-Commerce Project
 
 CREATE DATABASE ECommerce;
+
+USE ECommerce;
+
+CREATE TABLE shipping_dimen 
+(
+Ship_id nvarchar(20),
+Ship_Mode nvarchar(20) NOT NULL,
+Ship_Date date NOT NULL,
+PRIMARY KEY (Ship_id)
+);
+
+CREATE TABLE prod_dimen 
+(
+Prod_id nvarchar(20),
+Product_Category nvarchar(20) NOT NULL,
+Product_Sub_Category nvarchar(50) NOT NULL,
+PRIMARY KEY (Prod_id)
+);
+
+CREATE TABLE prod_dimen 
+(
+Prod_id nvarchar(20),
+Product_Category nvarchar(20) NOT NULL,
+Product_Sub_Category nvarchar(50) NOT NULL,
+PRIMARY KEY (Prod_id)
+);
