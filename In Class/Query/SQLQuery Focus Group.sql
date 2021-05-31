4b690053d62046f9e05c0826d5deeb54386a4368
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-05-31 20:37:35 +0300

    With Example

--- In Class/Query/SQLQuery Focus Group.sql
+++ In Class/Query/SQLQuery Focus Group.sql
@@ -1,5 +1,7 @@
 --FOCUS GROUP
 
+-- WITH 
+
 --En yüksek fiyatlý bisitletin markasý nedir?
 
 WITH T1 AS (
@@ -29,6 +31,30 @@ SELECT B.first_name, B.last_name
 FROM T3 A, [sales].[customers] B
 WHERE A.customer_id = B.customer_id
 
---
+-- How to query the factorial of 6 recursively?
 
+WITH factorial_func (step, factorial) AS
+		(SELECT 1, 1
+		UNION ALL
+		SELECT step + 1, (step + 1) * factorial
+		FROM factorial_func
+		WHERE step < 6 
+		)
+		
+SELECT step, factorial
+FROM factorial_func
 
+--2 way
+/*
+WITH factorial6 (step, result) AS (
+SELECT step = 0, result = 1
+UNION ALL
+SELECT step + 1, result = (step + 1) * result
+FROM factorial6
+WHERE step < 6
+)
+SELECT *
+FROM factorial6;
+*/
+
+-- VIEW
