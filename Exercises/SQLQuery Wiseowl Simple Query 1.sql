05f25389b5b6f8a5a73ab80f6fa43689fb7ea341
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-07-22 11:00:29 +0300

    Q4

--- Exercises/SQLQuery Wiseowl Simple Query 1.sql
+++ Exercises/SQLQuery Wiseowl Simple Query 1.sql
@@ -20,6 +20,13 @@ ORDER BY CategoryName DESC
 */
 
 -- How times change
--- 4)
-SELECT TOP(5) EventName AS What, EventDate AS [When] 
+-- 4) Create a query which uses two separate SELECT statements to show the first and last 2 events in date
+-- order from the tblEvent table:
+/*SELECT TOP(2) EventName AS What, EventDate AS [When] 
 FROM [dbo].[tblEvent]
+ORDER BY EventDate ASC
+
+SELECT TOP(2) EventName AS What, EventDate AS [When] 
+FROM [dbo].[tblEvent]
+ORDER BY EventDate DESC
+*/
