b1a7b3dcc5d05f021049d24b4a3a712b065875ac
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-07-22 11:02:27 +0300

    Added Question

--- Exercises/SQLQuery Wiseowl Simple Query 1.sql
+++ Exercises/SQLQuery Wiseowl Simple Query 1.sql
@@ -1,19 +1,21 @@
 -- WORLDEVENTS DATABASE (WISEOWL)
 -- What a way to start
--- 1)
+-- 1) Create a query to list out the following columns from the tblEvent table, with the most recent first (for which you'll need to use an 
+-- ORDER BY clause):
 /*SELECT EventName, EventDate  FROM [dbo].[tblEvent]
 Order by EventDate DESC
 */
 
 -- The dawn of histor
--- 2)
+-- 2) Write a query to show the first 5 events (in date order) from the tblEvent table:
 /*SELECT TOP(5) EventName AS What, EventDetails AS Details  
 FROM [dbo].[tblEvent]
 ORDER BY EventDate
 */
 
 -- Last 3 categories
--- 3)
+-- 3) Create a query to list out the id number and name of the last 3 categories from the tblCategory table in 
+-- alphabetical order:
 /*SELECT TOP(3) CategoryID, CategoryName 
 FROM [dbo].[tblCategory]
 ORDER BY CategoryName DESC
