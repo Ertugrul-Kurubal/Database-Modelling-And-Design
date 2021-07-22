965c03c0d204fccf4fb0abb3bf17a77817a3a0f7
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-07-22 10:54:03 +0300

    Q3

--- Exercises/SQLQuery Wiseowl Simple Query 1.sql
+++ Exercises/SQLQuery Wiseowl Simple Query 1.sql
@@ -1,13 +1,18 @@
 -- What a way to start
 -- 1)
 /*SELECT EventName, EventDate  FROM [dbo].[tblEvent]
-Order by EventDate DESC*/
+Order by EventDate DESC
+*/
 
 -- The dawn of histor
 -- 2)
-SELECT TOP(5) EventName AS What, EventDetails AS Details  
+/*SELECT TOP(5) EventName AS What, EventDetails AS Details  
 FROM [dbo].[tblEvent]
 ORDER BY EventDate
+*/
 
 -- Last 3 categories
 -- 3)
+SELECT TOP(3) CategoryID, CategoryName 
+FROM [dbo].[tblCategory]
+ORDER BY CategoryName DESC
