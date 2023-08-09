15fdaf423d43234d339cdfd2bb331da04df107ba
Ertuğrul Kurubal <antikakurubal@gmail.com>
2023-08-09 15:29:46 +0300

    this and that

---
+++ Exercises/Training/SQLQuery Calculations This And That 15.sql
@@ -0,0 +1,12 @@
+-- Use the CHARINDEX function multiple times to show the number of characters between two words in a text string
+
+SELECT e.EventName, e.EventDate,
+		CHARINDEX('this', e.EventDetails, 1) AS 'thisPosition',
+		CHARINDEX('that', e.EventDetails, 1) AS 'thatPosition',
+		CHARINDEX('that',e.EventDetails,1) - CHARINDEX('this',e.EventDetails,1) AS Offset
+
+FROM tblEvent AS e
+WHERE CHARINDEX('this', e.EventDetails, 1) > 1 AND
+CHARINDEX('that', e.EventDetails, 1) > 1 AND
+(CHARINDEX('that',e.EventDetails,1) - CHARINDEX('this',e.EventDetails,1)) > 1
+
