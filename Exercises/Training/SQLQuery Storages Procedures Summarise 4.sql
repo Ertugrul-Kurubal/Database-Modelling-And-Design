d225af75eb33c9774dea43e429218e0d2ea34f48
Ertuğrul Kurubal <antikakurubal@gmail.com>
2023-09-18 17:52:49 +0300

    storage param

--- Exercises/Training/SQLQuery Storages Procedures Summarise 4.sql
+++ Exercises/Training/SQLQuery Storages Procedures Summarise 4.sql
@@ -1,4 +1,6 @@
 -- Create a stored procedure to list Dr Who episodes by frequency in two ways
+USE DoctorWho
+GO
 
 /*
 CREATE PROC spSummariseEpisodes
@@ -18,17 +20,19 @@ ORDER BY 'Episodes' DESC
 EXEC spSummariseEpisodes
 */
 
-ALTER PROC spSummariseEpisodes
+ALTER PROC spSummariseEpisodes --@CompanionName nvarchar(30), @EnemyName nvarchar(30)
 AS
 SELECT TOP(5) c.CompanionName, COUNT(*) AS 'Episodes'
 FROM tblCompanion AS c
 	INNER JOIN tblEpisodeCompanion AS ec ON c.CompanionId = ec.CompanionId
+-- WHERE c.CompanionName = @CompanionName 
 GROUP BY c.CompanionName
 ORDER BY 'Episodes' DESC
 
 SELECT TOP(5) en.EnemyName, COUNT(*) AS 'Episodes'
 FROM tblEnemy AS en
 	INNER JOIN tblEpisodeEnemy AS ee ON en.EnemyId = ee.EnemyId
+-- WHERE en.EnemyName = @EnemyName
 GROUP BY en.EnemyName
 ORDER BY 'Episodes' DESC
 
