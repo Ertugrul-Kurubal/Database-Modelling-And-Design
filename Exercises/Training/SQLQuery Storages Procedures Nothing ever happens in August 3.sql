de0792bed467c56be325fd0ed798322a894b52e1
Ertuğrul Kurubal <antikakurubal@gmail.com>
2023-09-18 17:53:58 +0300

    storage folder

--- Exercises/Training/SQLQuery Storages Procedures Nothing ever happens in August 3.sql
+++
@@ -1,7 +0,0 @@
--- Filter the select statement, only show events occuring in August.
-
-CREATE PROC uspAugustEvents 
-AS
-SELECT e.EventID, e.EventName, e.EventDetails, e.EventDate
-FROM tblEvent AS e
-WHERE MONTH(e.EventDate) = 8 --DATENAME(MM,EventDate) LIKE 'August'
